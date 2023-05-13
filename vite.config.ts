6a9ba6fd37cf69181e64c52e0577fa574528fe0b
M Abrar Khalid <iabrarkhalidofficial@gmail.com>
2023-05-14 03:44:02 +0500

    plugin remove

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from "vite";
 import preload from "vite-plugin-preload";
 import react from "@vitejs/plugin-react";
 import viteCompression from "vite-plugin-compression";
-import viteImagemin from "vite-plugin-imagemin";
 
 export default defineConfig({
   resolve: {
@@ -35,54 +34,6 @@ export default defineConfig({
       algorithm: "brotliCompress",
       threshold: 100,
     }),
-    viteImagemin({
-      gifsicle: {
-        optimizationLevel: 7,
-        interlaced: false,
-      },
-      optipng: {
-        optimizationLevel: 7,
-      },
-      mozjpeg: {
-        quality: 30,
-      },
-      pngquant: {
-        quality: [0.7, 0.8],
-        speed: 4,
-      },
-      webp: {
-        quality: 70,
-      },
-      svgo: {
-        multipass: true,
-        plugins: [
-          {
-            name: "removeViewBox",
-          },
-          {
-            name: "minifyStyles",
-          },
-          {
-            name: "removeMetadata",
-          },
-          {
-            name: "removeUselessStrokeAndFill",
-          },
-          {
-            name: "reusePaths",
-          },
-          {
-            name: "removeEmptyAttrs",
-            active: true,
-          },
-        ],
-      },
-    }),
     react(),
   ],
-  build: {
-    target: "es2018",
-    cssCodeSplit: true,
-    assetsInlineLimit: 4096,
-  },
 });
